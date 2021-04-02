ac169cf1f0eddf405ca2fbe24c8530b664a4d0b8
kymkyj <ymyj33@naver.com>
2021-04-03 00:21:46 +0900

    [feat] 4/3 inf algo commit kym

--- src/infString2.java
+++ src/infString2.java
@@ -6,10 +6,11 @@ public class infString2 {
     public static String solution(String str){
         String answer = "";
         for(char x : str.toCharArray()){
-            if(Character.isLowerCase(x)){
-                answer += Character.toUpperCase(x);
-            }else {
-                answer += Character.toLowerCase(x);
+           // 아스키코드 방법 내가 생각한 것
+            if(x>= 65 && x <= 90){
+                answer += (char)(x + 32);
+            }else{
+                answer += (char)(x - 32);
             }
         }
         return answer;
