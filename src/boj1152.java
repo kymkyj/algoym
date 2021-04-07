53cdf83489782b2b698559109d2c9d375d2aefdd
kymkyj <ymyj33@naver.com>
2021-04-08 00:41:38 +0900

    [feat] 4/7 algo commit kym

---
+++ src/boj1152.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 단어의 개수
+public class boj1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(Solution(str));
+    }
+    public static int Solution(String str){
+        StringTokenizer st = new StringTokenizer(str, " ");
+        return st.countTokens();
+    }
+}
