db8fb7e9e8548de238ebec608a666d5f0daca993
kymkyj <ymyj33@naver.com>
2021-04-09 00:12:30 +0900

    [feat] 4/8 algo commit kym

--- src/infString11.java
+++ src/infString11.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 문자열 압축(카카오 기출)
+// 문자열 압축
 public class infString11 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +14,7 @@ public class infString11 {
         int cnt = 1; // 카운트 해줄 변수
         str = str + " "; // 마지막에 비교해줄 문자가 없으므로 공백을 넣어줌 안그러면 index out of 에러남
         String answer = "";
-        for(int i=0; i<str.length()-1; i++){
+        for(int i=0; i<str.length()-1; i++){ // i가 마지막까지 가면 안되서 length()에 -1을 해준다.
             if(str.charAt(i) == str.charAt(i+1)){ // K랑 K랑 같냐?
                 cnt++;
                 continue;
