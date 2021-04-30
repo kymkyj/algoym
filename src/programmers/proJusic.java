1c04d4d525c066ed50f3d3edf8ccd43496912e6f
kymkyj <ymyj33@naver.com>
2021-04-30 23:04:26 +0900

    [feat] 4/30 algo commit kym

--- src/programmers/proJusic.java
+++ src/programmers/proJusic.java
@@ -1,9 +1,20 @@
 package programmers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] prices) {
+
+
+class projusic {
+    public static void main(String[] args) {
+        int [] prices = {1,2,3,2,3};
+        for(int i : solution(prices)){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] solution(int[] prices) {
         // prices 주식가격이 담긴 배열
         int[] answer = new int[prices.length]; // 가격의 기간을 담을 배열
         int count=0;
