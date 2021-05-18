b32ca5f841fd370984233ea558af0fe102e5aee8
kymkyj <ymyj33@naver.com>
2021-05-18 23:44:26 +0900

    [feat] 5/18 algo commit kym

---
+++ src/inflearn/infInjupList.java
@@ -0,0 +1,43 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 인접리스트
+public class infInjupList {
+    static int n, m, answer=0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
+    public void DFS(int v){
+        if(v==n) answer++;
+        else{
+            for(int nv : graph.get(v)) {
+                if (ch[nv] == 0){
+                    ch[nv]=1;
+                    DFS(nv);
+                    ch[nv]=0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        infInjupList T = new infInjupList();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        for(int i=0; i<=n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n+1];
+        for(int i=0; i<m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+
+    }
+}
