eabfd04116141e7b6c0341b7a53514386ca48a1c
kymkyj <ymyj33@naver.com>
2021-05-19 00:01:12 +0900

    [feat] 5/18 algo commit kym

--- src/inflearn/infCourseSearch.java
+++ src/inflearn/infCourseSearch.java
@@ -7,17 +7,18 @@ import java.util.StringTokenizer;
 
 // 경로탐색(DFS)
 // 인접행렬로 푼 방식
+// 그래프에서는 한번 방문한 노드는 방문하는 것이 아니다!
 public class infCourseSearch {
     static int n, m, answer=0; // 전역변수 static -> main에서도 입력받고 해야하기때문에
     static int[][] graph;
     static int[] ch;
-    public void DFS(int v){
+    public void DFS(int v){ // v는 1로시작
         if (v == n) answer++;
         else{
             for(int i=1; i<=n; i++){ // 정점번호
                 if(graph[v][i] == 1 && ch[i]==0){ // v에서 i로 이동하는 모든경우 구하기 단 체크가 안되어있는 곳만!
                     ch[i] = 1; // 이동하고 체크해준다
-                    DFS(i);
+                    DFS(i); // v에서 i로 이동하는 것이라서 i로 적어줘야한다.
                     ch[i]=0; // 호출을 위해서 하면 체크되고 여기선 반대로 돌아가는 경우로 체크 해제
                 }
             }
