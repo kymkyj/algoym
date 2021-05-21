dc63aec1e94fe98b5a00ce604e4019db60bcf19c
kymkyj <ymyj33@naver.com>
2021-05-22 00:10:55 +0900

    [feat] 5/21 algo commit kym

---
+++ src/programmers/proOneCache.java
@@ -0,0 +1,42 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 프로그래머스 1차캐시
+public class proOneCache {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0; // 캐시 수행시간 카운트
+        String [] cache = new String[cacheSize];
+        for(String s : cities){
+            int pos = -1;
+            for(int i=0; i<cacheSize; i++){
+                if(s==cache[i]) {
+                    pos = i; // hit
+                }
+            }
+            if(pos==-1){ // miss
+                for(int j=cache.length-1; j>=1; j--){
+                    cache[j] = cache[j-1];
+                }
+                answer = answer + 5;
+            }else{ // hit
+                for(int j=pos; j>=1; j--){
+                    cache[j] = cache[j-1];
+                }
+                answer++;
+            }
+            cache[0] = s;
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        //String [] citys = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        String [] citys = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+        proOneCache T = new proOneCache();
+        System.out.println(T.solution(n, citys));
+    }
+}
