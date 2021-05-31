b3edc15b384c9ca3e71e8b879df6f07018d1d051
kymkyj <ymyj33@naver.com>
2021-05-31 23:40:57 +0900

    [feat] 5/31 algo commit kym

--- src/boj/boj1697.java
+++ src/boj/boj1697.java
@@ -11,35 +11,39 @@ import java.util.StringTokenizer;
 public class boj1697 {
    static int n, k;
    static int [] ch; // 들린지 체크하기위해
-   static int [] dis;
-     public void BFS(int n){
-        int L = 0; // 노드 레벨증가 값
-        ch = new int[10001]; // 걍 큰값잡기
-        dis = new int[3];
-        ch[n] = 1; // 5번위치(수빈위치) 들렸다고 1찍기
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(n); // 수빈위치 지정
-        while(!queue.isEmpty()){
-            int len = queue.size();
-            for(int i=0; i<len; i++){
-                int x = queue.poll();
-                if(x == k) System.out.println(L);
-                else {
-                    for (int j = 0; j < 3; j++) {
-                        int nx = 0;
-                        if (j == 0) {
-                            dis[j] = nx + 1;
-                        } else if (j == 1) {
-                            dis[j] = nx - 1;
-                        } else {
-                            dis[j] = nx * 2;
-                        }
-                    }
-                }
-            }
-            L++;
-        }
-    }
+   Queue<Integer> q = new LinkedList<>();
+   public int BFS(int n, int k){
+       ch = new int[100001];
+       ch[n] = 1;
+       q.offer(n);
+       int L = 0;
+       while(!q.isEmpty()){
+           // 여기서부터 레벨탐색을 해야함
+           int len = q.size(); // 처음에는 수빈이 위치 하나로 len은 1
+           for(int i=0; i<len; i++){
+               int x = q.poll(); // 원소를 꺼낸다.
+//               if(x == k) return L; // 이 경우는 큐에 집어넣었다가 꺼내서 보는 경우
+               for(int j=0; j<3; j++){ // j는 세 방법으로 걸을 때(x-1, x+1) 순간이동(x * 2)
+                   // nx는 x의 자식노드
+                   int nx = 0;
+                   if(j == 0){
+                       nx = x - 1;
+                   }else if(j == 1){
+                       nx = x + 1;
+                   }else{
+                       nx = x * 2;
+                   }
+                   if(nx >= 1 && nx <= 100000 && ch[nx] == 0){
+                       ch[nx] = 1;
+                       q.offer(nx);
+                   }
+                   if(nx == k) return L+1; // 큐에 집어넣을려는 값이 k인가를 보는게 더 낫다
+               }
+           }
+           L++;
+       }
+       return 0;
+   }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -47,6 +51,6 @@ public class boj1697 {
         int n = Integer.parseInt(st.nextToken()); // 수빈위치
         int k = Integer.parseInt(st.nextToken()); // 동생위치
         boj1697 T = new boj1697();
-        T.BFS(n);
+        System.out.println(T.BFS(n, k));
     }
 }
