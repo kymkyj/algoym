48bc94a4ebad26d1c3143bf4295b53103a372268
kymkyj <ymyj33@naver.com>
2021-06-16 00:00:37 +0900

    [feat] 6/15 algo commit kym 응급실 수정

--- src/inflearn/infAmbulance.java
+++ src/inflearn/infAmbulance.java
@@ -3,6 +3,8 @@ package inflearn;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 응급실
@@ -10,19 +12,37 @@ import java.util.StringTokenizer;
     5 2                 3
     60 50 70 80 90
  */
+class Person{
+    int id;
+    int priority;
+    public Person(int id, int priority){
+        this.id = id;
+        this.priority=priority;
+    }
+}
+
 public class infAmbulance {
     public static int solution(int n, int m, int [] person){
-        int answer = 1; // 순서 카운트
-        int onePerson = person[m];
-        for(int i=0; i<n; i++){
-            if(i == m) {
-                return i;
-            }
-            if(person[i] >= onePerson) {
-                answer++;
-            }
-        }
-        return answer;
+       int answer = 0;
+       Queue<Person> Q = new LinkedList<>();
+       for(int i=0; i<n; i++){
+           Q.offer(new Person(i, person[i]));
+       }
+       while(!Q.isEmpty()){
+           Person tmp = Q.poll();
+           for(Person x : Q){
+               if(x.priority > tmp.priority){
+                   Q.offer(tmp);
+                   tmp = null;
+                   break;
+               }
+           }
+           if(tmp != null){
+               answer++;
+               if(tmp.id == m) return answer;
+           }
+       }
+       return answer;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
