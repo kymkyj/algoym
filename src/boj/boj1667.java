41ac7517bc3847e74b17a44be5c89c680da96b5e
kymkyj <ymyj33@naver.com>
2021-06-28 23:11:37 +0900

    [feat] 6/28 algo commit kym

---
+++ src/boj/boj1667.java
@@ -0,0 +1,66 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 단지번호 붙이기
+/*
+    7        ->   3 7 8 9
+    0110100
+    0110101
+    1110101
+    0000111
+    0100000
+    0111110
+    0111000
+
+ */
+public class boj1667 {
+    static int n, totalCnt, dangiCnt;
+    static int [] dx = {-1, 0, 1, 0};
+    static int [] dy = {0, 1, 0, -1};
+    static int [][] dangi, ch;
+    static ArrayList<Integer> list = new ArrayList<>();
+    public void DFS(int x, int y){
+        dangi[x][y] = 0;
+        ch[x][y] = 1; // 간 단지 체크
+        if(x == n && y == n) totalCnt++;
+        else{
+            for(int i=0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y+ dy[i];
+                if(nx >= 0 && nx <= n && ny >=0 && ny <= n && dangi[nx][ny] == 1){
+                    if(ch[x][y] != 1){
+                        ch[nx][ny] = 1;
+                        dangiCnt++;
+                        DFS(nx, ny);
+                        ch[nx][ny] = 0;
+                    }
+                }
+            }
+            list.add(dangiCnt);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        boj1667 T = new boj1667();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        dangi = new int[n][n];
+        ch = new int[n][n];
+        for(int i=0; i<n; i++){
+            String tmp = sc.next();
+            for(int j=0; i<n; j++){
+                dangi[i][j] = sc.nextInt();
+            }
+        }
+        T.DFS(0, 0);
+        for(int i : list){
+            System.out.println(i);
+        }
+    }
+}
