3c2b4a9f839921877863dfe9d6a0c10f9382ee99
kymkyj <ymyj33@naver.com>
2021-06-29 22:32:08 +0900

    [feat] 6/29 algo commit kym 왜 런타임 에러나지..

--- src/boj/boj2667.java
+++ src/boj/boj2667.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.Collections;
 
 // 단지번호 붙이기
 /*
@@ -19,51 +18,56 @@ import java.util.StringTokenizer;
     0111000
 
  */
-public class boj1667 {
-    static int n, totalCnt, dangiCnt;
+public class boj2667 {
+    static int n, dangiCnt;
     static int [] dx = {-1, 0, 1, 0};
     static int [] dy = {0, 1, 0, -1};
-    static int [][] dangi, ch;
+    static int [][] dangi;
+    static boolean [][] ch;
     static ArrayList<Integer> list = new ArrayList<>();
-    public static void DFS(int x, int y){
-        ch[x][y] = 1; // 간 단지 체크
-        if(x == n && y == n) totalCnt++;
-        else{
+
+    public int DFS(int x, int y){
+        ch[x][y] = true; // 간 단지 체크
             for(int i=0; i<4; i++){
                 int nx = x + dx[i];
                 int ny = y+ dy[i];
-                if(nx >= 0 && nx <= n && ny >=0 && ny <= n && dangi[nx][ny] == 1){
-                    if(ch[x][y] != 1){
-                        ch[nx][ny] = 1;
+                if(nx >= 0 && nx <= n && ny >=0 && ny <= n){
+                    if(dangi[nx][ny] == 1 && !ch[nx][ny]){
+                        ch[nx][ny] = true;
                         dangiCnt++;
                         DFS(nx, ny);
-                        ch[nx][ny] = 0;
                     }
                 }
             }
-            list.add(dangiCnt);
-        }
+        return dangiCnt;
     }
 
     public static void main(String[] args) throws IOException {
-        boj1667 T = new boj1667();
+        boj2667 T = new boj2667();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         dangi = new int[n][n];
-        ch = new int[n][n];
+        ch = new boolean[n][n];
         for(int i=0; i<n; i++){
             String tmp = br.readLine();
             for(int j=0; i<n; j++){
-                dangi[i][j] = tmp.charAt(j) - '0';
+                dangi[i][j] = tmp.charAt(j)-'0';
             }
         }
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(dangi[i][j] == 1 && ch[i][j] != 1){
-                    dangiCnt++;
-                    DFS(i,j);
+                if(dangi[i][j] == 1 && !ch[i][j]){
+                    dangiCnt = 1;
+                    T.DFS(i,j);
+                    list.add(dangiCnt);
                 }
             }
         }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+
+        for(int i=0;i<list.size();i++)
+            System.out.println(list.get(i));
     }
 }
