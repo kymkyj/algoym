82c97c2b622e4e1d918849213210078b94d70ce3
kymkyj <ymyj33@naver.com>
2021-07-06 23:28:27 +0900

    [feat] 7/6 algo commit kym

--- src/boj/boj21317.java
+++ src/boj/boj21317.java
@@ -17,15 +17,29 @@ import java.util.StringTokenizer;
 public class boj21317 {
     static int n, k;
     static int [][] bridge;
-    static int min = Integer.MAX_VALUE;
-
+    static int answer = Integer.MAX_VALUE;
+    public void DFS(int L, int sum){
+        if(L > n) return;
+        if(L == n){
+            answer = Math.min(answer, sum);
+            return;
+        }else{
+            DFS(L, sum + bridge[L][0]);
+            DFS(L, sum + bridge[L][1]);
+        }
+    }
     public static void main(String[] args) throws IOException {
+        boj21317 T = new boj21317();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         bridge = new int[n][n];
         StringTokenizer st = null;
-        for(int i=0; i<n; i++){
-
+        for(int i=1; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            bridge[i][0] = Integer.parseInt(st.nextToken());
+            bridge[i][1] = Integer.parseInt(st.nextToken());
         }
+        T.DFS(1, 0); // 1번 돌부터 시작한다고 한거랑 0은 합계
+        System.out.println(answer);
     }
 }
