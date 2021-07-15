030d1e4a47f78893c700136da8b1a46faf1f378d
kymkyj <ymyj33@naver.com>
2021-07-15 23:56:41 +0900

    [feat] 7/15 algo commit kym

--- src/boj/boj2110.java
+++ src/boj/boj2110.java
@@ -1,11 +1,53 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 공유기 설치
+/*
+    5 3
+    1
+    2
+    8
+    4
+    9
+ */
 public class boj2110 {
-    static int [] dy;
-    public static int solution(){
-
-    }
-    public static void main(String[] args) {
-
+    static int lt, rt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 집의 수
+        int c = Integer.parseInt(st.nextToken()); // 공유기 수
+        int [] arr = new int[n];
+        int answer = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        lt = 1; //최소 길이
+        rt = arr[n - 1] - arr[0];// 최대 길이
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2; //공유기 거리 기준
+            int firstModem = arr[0]; //첫 위치에 설치
+            int count = 1; //공유기 설치 개수
+            for (int i = 0; i < n; i++) {
+                int distance = arr[i] - firstModem;
+                if (distance >= mid) { //거리차가 기준보다 이상되야 설치 가능
+                    count++;
+                    firstModem = arr[i];
+                }
+            }
+            if (count >= c) { //공유기 설치가 더 많이 됬으니 간격을 넓혀서 줄여야함
+                lt = mid + 1;
+                answer = mid;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        System.out.println(answer);
     }
 }
